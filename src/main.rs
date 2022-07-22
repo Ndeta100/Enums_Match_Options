b48059219e43c2d0687c06ba6a261081c8168f19
Ndeta Innocent <you@example.com>
2022-07-22 17:12:25 +0300

    using match and enums together

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,43 @@ enum Message {
     Write(String),
     ChangeColor(u32, u32, u32),
 }
-// struct IpAddr {
-//     kind: IpAddrKind,
-//     address: String,
-// }
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
 fn route(ip_kind: IpAddrKind) {}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter {:?}", state);
+            25
+        }
+    }
+}
+//Matching with option<T>
+
 fn main() {
-    println!("Hello, world!");
     let four = IpAddrKind::v4;
     let six = IpAddrKind::v6;
-
+    let penny = Coin::Penny;
+    let nickel = value_in_cents(penny);
+    let alaska = Coin::Quarter(UsState::Alaska);
+    let results = value_in_cents(alaska);
+    println!("value in nickel is {:?}", results);
     // let home =IpAddr{
     //     kind:IpAddrKind::v4,
     //     address:String::from("Katusepapi")
